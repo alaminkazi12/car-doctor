327c98222d863084fa790d792c00a2848cda6b72
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-07-13 01:35:37 +0600

    added nav

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className="btn btn-secondary">Secondary</button>
-    </main>
-  );
+  return <main className="h-screen">Home</main>;
 }
