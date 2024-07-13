7571959f48e2bf43a8d20a14525e7fc8e01d56a7
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-07-13 13:47:16 +0600

    added navbar style

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { IoBagHandleOutline } from "react-icons/io5";
 
 const navItems = [
   {
@@ -27,76 +29,78 @@ const navItems = [
 
 const Navbar = () => {
   return (
-    <div className="container mx-auto pt-2">
-      <div className="navbar mt-10">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <div className=" bg-base-100">
+      <div className="container mx-auto md:pt-2">
+        <div className="navbar  text-black/90">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <div
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                {navItems.map((item) => (
+                  <Link
+                    className="font-semibold hover:text-primary"
+                    key={item.path}
+                    href={item.path}
+                  >
+                    {item.title}
+                  </Link>
+                ))}
+              </div>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+            <Link href="/">
+              <Image
+                priority
+                src="/assets/logo.svg"
+                height={60}
+                width={100}
+                alt="car doctor logo"
+              />
+            </Link>
           </div>
-          <Link href="/">
-            <Image
-              priority
-              src="/assets/logo.svg"
-              height={60}
-              width={100}
-              alt="car doctor logo"
-            />
-          </Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <div className="text-white flex items-center gap-6">
-            {navItems.map((item) => (
-              <Link
-                className="font-semibold hover:text-primary"
-                key={item.path}
-                href={item.path}
-              >
-                {item.title}
-              </Link>
-            ))}
+          <div className="navbar-center hidden lg:flex">
+            <div className=" flex items-center gap-6">
+              {navItems.map((item) => (
+                <Link
+                  className="font-semibold  text-xl hover:text-primary"
+                  key={item.path}
+                  href={item.path}
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="navbar-end">
-          <a className="btn btn-outline px-6 py-4 text-primary border-2 ">
-            Appointment
-          </a>
+          <div className="navbar-end">
+            <div className="flex items-center text-2xl text-white gap-3 pr-4">
+              <IoBagHandleOutline />
+              <CiSearch />
+            </div>
+            <a className="btn btn-outline px-6 py-4 text-primary border-2 ">
+              Appointment
+            </a>
+          </div>
         </div>
       </div>
     </div>
